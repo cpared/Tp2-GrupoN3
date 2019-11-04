8e00f5891cbfa8ad9f53eb47195972539500a108
Ignacio <ignacioclugones@gmail.com>
2019-11-04 00:07:12 -0300

    Changed how the board receive the cell, so it no longers need the OBJECT instead it needs the position (as which row and column is it placed). Changed the method distance, so it is working and clearer to understand. Created a getter for cell which needs the row and column.
    Signed-off-by: Ignacio <ignacioclugones@gmail.com>

--- src/main/board/board.java
+++ src/main/board/board.java
@@ -1,8 +1,19 @@
+import java.util.ArrayList;
+import java.lang.Math;
+
 class Board {
-    public void movePiece(Cell originCell, Cell destinationCell) {
-        if (distance(originCell,destinationCell) < 1) {
+    private ArrayList<ArrayList> cellArray;
+    public void placePiece(Piece piece,int row, int column){
+        //smt to check teams or zones, to discuss with the team
+        Cell cell = this.getCell(row,column);
+        cell.putPieceInCell(piece);
+    }
+    public void movePiece(int firstRow,int firstColumn,int secondRow,int secondColumn) {
+        if (distance(firstRow,firstColumn,secondRow,secondColumn) > 1) {
             throw Exception CanNotMakeThatMoveException;
         }
+        Cell originCell = this.getCell(firstRow,firstColumn);
+        Cell destinationCell = this.getCell(secondRow,secondColumn);
         Piece piece;
         piece = originCell.deletePieceFromCell();
         try {
@@ -13,6 +24,13 @@ class Board {
         }
     }
 
-    private int distance(Cell originCell, Cell destinationCell) {
+    private Cell getCell(int row, int column) {
+        return (Cell) this.cellArray.get(row).get(column);
     }
+
+    private int distance(int firstRow, int firstColumn, int secondRow, int secondColumn) {
+        return Math.max(Math.abs(firstRow - secondRow), Math.abs(secondColumn - firstColumn));
+    }
+
+
 }
