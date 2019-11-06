b02f6a25b2fd7a7b46622a6479df3d2a581fb3b7
Sol <mperezfreire@fi.uba.ar>
2019-11-06 17:30:49 -0300

    Board class fixed

--- src/main/java/board/Board.java
+++ src/main/java/board/Board.java
@@ -3,10 +3,10 @@ import piece.Piece;
 import java.util.ArrayList;
 import java.lang.Math;
 
-class Board {
+public class Board {
     private ArrayList<ArrayList<Cell>> cellArray;
 
-    Board() {
+    public Board() {
         cellArray = new ArrayList<ArrayList<Cell>>();
         for (int i = 0; i < 20; i += 1){
             this.cellArray.add(new ArrayList<Cell>());
