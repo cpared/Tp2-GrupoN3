9010f9a513ffbb2b3c8bf5e485ca0fe6aa15a233
Sol <mperezfreire@fi.uba.ar>
2019-11-06 18:13:04 -0300

    Added integration tests

--- src/test/java/IntegrationTests/IntegrationTest.java
+++ src/test/java/IntegrationTests/IntegrationTest.java
@@ -1,11 +1,49 @@
 package IntegrationTests;
 
+import board.Board;
+import board.CanNotMakeThatMoveException;
 import org.junit.jupiter.api.Test;
+import piece.Piece;
 import player.Player;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class IntegrationTest {
+
+    @Test
+    void test00CanMoveAPieceFromRow3AndColumn3InAllPossibleWays(){
+
+        Board board = new Board();
+        Piece piece = new Piece();
+
+        board.placePiece(piece,3,3);
+        for (int i =2;i<5;i++) {
+            for (int j = 2; j < 5; j++) {
+                if (i == 3 && j == 3) {
+                    continue;
+                }
+                board.movePiece(3, 3, i, j);
+                board.movePiece(i, j, 3, 3);
+            }
+        }
+    }
+
+    @Test
+    void test01APieceCanNotMoveToAnOccupiedCell(){
+        Board board = new Board();
+        Piece piece = new Piece();
+
+        board.placePiece(piece,3,3);
+        board.placePiece(piece,3,4);
+
+        try{
+            board.movePiece(3,3,3,4);
+            fail();
+        }
+        catch (CanNotMakeThatMoveException e){
+            assert true;
+        }
+    }
 
     @Test
     public void test11PlayerCantChooseMorePiecesThanWhatHisPointsAllow () {
