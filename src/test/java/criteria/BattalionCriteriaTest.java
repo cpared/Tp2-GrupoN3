c951ef90806a145d68d07e643a0ede89c1b7d799
Eugenia Vullioud <evullioud@fi.uba.ar>
2019-11-15 14:41:07 -0300

    added test to check that trying to make a battalion with 3 similar pieces that are not soldiers will not create battalio

--- src/test/java/criteria/BattalionCriteriaTest.java
+++ src/test/java/criteria/BattalionCriteriaTest.java
@@ -19,21 +19,18 @@ class BattalionCriteriaTest {
     private Piece soldier2 = factory.createSoldier ();
     private Piece soldier3 = factory.createSoldier ();
     private Piece soldier4 = factory.createSoldier ();
-    private Piece rider1 = factory.createRider ();
-    private Piece rider2 = factory.createRider ();
-    private Piece healer1 = factory.createHealer ();
-    private Piece healer2 = factory.createHealer ();
 
     @Test
     void test01FilteringByBattalionCriteriaLeavesSoldiers (){
         //Assemble
+        Piece rider1 = factory.createRider ();
+        Piece rider2 = factory.createRider ();
+
         pieces.add ( soldier1 );
         pieces.add ( soldier2 );
         pieces.add ( soldier3 );
         pieces.add ( rider1 );
         pieces.add ( rider2 );
-        pieces.add ( healer1 );
-        pieces.add ( healer2 );
 
         //Act
         BattalionCriteria battalionPieces = new BattalionCriteria ();
@@ -46,12 +43,13 @@ class BattalionCriteriaTest {
     }
 
     @Test
-    void test02FilteringByHealerCriteriaLeaves2Healers (){
+    void test02FilteringByBattalionCriteriaLeaves3oldiers (){
         //Assemble
+        Piece rider1 = factory.createRider ();
+        Piece rider2 = factory.createRider ();
+
         pieces.add ( rider1 );
         pieces.add ( rider2 );
-        pieces.add ( healer1 );
-        pieces.add ( healer2 );
         pieces.add ( soldier1 );
         pieces.add ( soldier2 );
         pieces.add ( soldier3 );
@@ -67,10 +65,12 @@ class BattalionCriteriaTest {
     @Test
     void test03TryingToMakeABattalionWith4SoldiersWillOnlyAllow3Soldiers (){
         //Assemble
+
+        Piece rider1 = factory.createRider ();
+        Piece rider2 = factory.createRider ();
+
         pieces.add ( rider1 );
         pieces.add ( rider2 );
-        pieces.add ( healer1 );
-        pieces.add ( healer2 );
         pieces.add ( soldier1 );
         pieces.add ( soldier2 );
         pieces.add ( soldier3 );
@@ -87,10 +87,11 @@ class BattalionCriteriaTest {
     @Test
     void test04TryingToMakeABattalionWith2SoldiersWillNotCreateBattalion (){
         //Assemble
+        Piece rider1 = factory.createRider ();
+        Piece rider2 = factory.createRider ();
+
         pieces.add ( rider1 );
         pieces.add ( rider2 );
-        pieces.add ( healer1 );
-        pieces.add ( healer2 );
         pieces.add ( soldier1 );
         pieces.add ( soldier2 );
 
@@ -103,12 +104,13 @@ class BattalionCriteriaTest {
     }
 
     @Test
-    void test04TryingToMakeABattalionWith1SoldierWillNotCreateBattalion (){
+    void test05TryingToMakeABattalionWith1SoldierWillNotCreateBattalion (){
         //Assemble
+        Piece rider1 = factory.createRider ();
+        Piece rider2 = factory.createRider ();
+
         pieces.add ( rider1 );
         pieces.add ( rider2 );
-        pieces.add ( healer1 );
-        pieces.add ( healer2 );
         pieces.add ( soldier1 );
 
         //Act
@@ -117,6 +119,90 @@ class BattalionCriteriaTest {
 
         //Assert
         assertEquals ( 0, battalion.size() );
+    }
+
+    @Test
+    void test06TryingToMakeABattalionWith3RidersWillNotCreateBattalion (){
+        //Assemble
+        Piece rider1 = factory.createRider ();
+        Piece rider2 = factory.createRider ();
+        Piece rider3 = factory.createRider ();
+
+        pieces.add ( rider1 );
+        pieces.add ( rider2 );
+        pieces.add ( rider3 );
+        pieces.add ( soldier1 );
+
+        //Act
+        BattalionCriteria battalionPieces = new BattalionCriteria ();
+        ArrayList<Piece> battalion = battalionPieces.criteria ( pieces );
+
+        //Assert
+        assertEquals ( 0, battalion.size() );
+    }
+
+    @Test
+    void test08TryingToMakeABattalionWith3HealersWillNotCreateBattalion (){
+        //Assemble
+        Piece healer3 = factory.createHealer ();
+        Piece healer1 = factory.createHealer ();
+        Piece healer2 = factory.createHealer ();
+
+        pieces.add ( soldier1 );
+        pieces.add ( soldier2 );
+        pieces.add ( healer1 );
+        pieces.add ( healer2 );
+        pieces.add ( healer3 );
+
+        //Act
+        BattalionCriteria battalionPieces = new BattalionCriteria ();
+        ArrayList<Piece> battalion = battalionPieces.criteria ( pieces );
+
+        //Assert
+        assertEquals ( 0, battalion.size() );
+    }
+
+    @Test
+    void test09TryingToMakeABattalionWith3CatapultsWillNotCreateBattalion (){
+        //Assemble
+        Piece catapult3 = factory.createCatapult ();
+        Piece catapult2 = factory.createCatapult ();
+        Piece catapult1 = factory.createCatapult ();
+
+        pieces.add ( soldier1 );
+        pieces.add ( soldier2 );
+        pieces.add ( catapult1 );
+        pieces.add ( catapult2 );
+        pieces.add ( catapult3 );
+
+        //Act
+        BattalionCriteria battalionPieces = new BattalionCriteria ();
+        ArrayList<Piece> battalion = battalionPieces.criteria ( pieces );
+
+        //Assert
+        assertEquals ( 0, battalion.size() );
+    }
+
+    @Test
+    void test10TryingToMakeABattalionWith3CatapultsAnd3SoldiersWillCreateBattalionWithSoldiers (){
+        //Assemble
+        Piece catapult3 = factory.createCatapult ();
+        Piece catapult2 = factory.createCatapult ();
+        Piece catapult1 = factory.createCatapult ();
+
+        pieces.add ( soldier1 );
+        pieces.add ( soldier2 );
+        pieces.add ( catapult1 );
+        pieces.add ( catapult2 );
+        pieces.add ( catapult3 );
+        pieces.add ( soldier3 );
+
+        //Act
+        BattalionCriteria battalionPieces = new BattalionCriteria ();
+        ArrayList<Piece> battalion = battalionPieces.criteria ( pieces );
+
+        //Assert
+        assertEquals ( 3, battalion.size() );
     }
 
 }
