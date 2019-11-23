08b00b96ee4395ceff42cf5f0731d2d6db1e8784
Eugenia Vullioud <evullioud@fi.uba.ar>
2019-11-23 16:49:57 -0300

    factory refactor

--- src/main/java/piece/PieceFactory.java
+++ src/main/java/piece/PieceFactory.java
@@ -6,43 +6,49 @@ import team.Team;
 public class PieceFactory {
     private Team team;
     private int playerPoints;
+    private int soldierCost;
+    private int healerCost;
+    private int riderCost;
+    private int catapultCost;
 
     public PieceFactory ( Team team ) {
         this.team = team;
         playerPoints = 20;
+        soldierCost = 1;
+        this.healerCost = 2;
+        this.riderCost = 3;
+        this.catapultCost = 5;
     }
 
     public Piece createSoldier () throws PlayerHas20PointsOnlyException {
 
-        int cost = 1;
-        if (cost > this.playerPoints) throw new PlayerHas20PointsOnlyException ();
+        if (this.soldierCost > this.playerPoints) throw new PlayerHas20PointsOnlyException ();
         Piece soldier = new Soldier ( this.team );
-        this.playerPoints -= cost;
+        this.playerPoints -= this.soldierCost;
         return soldier;
     }
 
     public Piece createHealer () throws PlayerHas20PointsOnlyException {
 
-        int cost = 2;
-        if (cost > this.playerPoints) throw new PlayerHas20PointsOnlyException ();
+        if (this.healerCost > this.playerPoints) throw new PlayerHas20PointsOnlyException ();
         Piece healer = new Healer ( this.team );
-        this.playerPoints -= cost;
+        this.playerPoints -= this.healerCost;
         return healer;
     }
 
     public Piece createRider () throws PlayerHas20PointsOnlyException {
-        int cost = 3;
-        if (cost > this.playerPoints) throw new PlayerHas20PointsOnlyException ();
+
+        if (this.riderCost > this.playerPoints) throw new PlayerHas20PointsOnlyException ();
         Piece rider = new Rider ( this.team );
-        this.playerPoints -= cost;
+        this.playerPoints -= this.riderCost;
         return rider;
     }
 
     public Piece createCatapult () throws PlayerHas20PointsOnlyException {
-        int cost = 5;
-        if (cost > this.playerPoints) throw new PlayerHas20PointsOnlyException ();
+
+        if (this.catapultCost > this.playerPoints) throw new PlayerHas20PointsOnlyException ();
         Piece rider = new Catapult ( this.team );
-        this.playerPoints -= cost;
+        this.playerPoints -= this.catapultCost;
         return rider;
     }
 }
